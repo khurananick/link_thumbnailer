6ebe4e061924cb38dcf2d6b6e1484c56c72709e1
Nick <khurananick@gmail.com>
2017-06-29 16:07:03 -0400

    trying article: instead of og:

--- lib/link_thumbnailer/scrapers/opengraph/section.rb
+++ lib/link_thumbnailer/scrapers/opengraph/section.rb
@@ -4,6 +4,39 @@ module LinkThumbnailer
   module Scrapers
     module Opengraph
       class Section < ::LinkThumbnailer::Scrapers::Opengraph::Base
+
+        def applicable?
+          meta.any? { |node| opengraph_node?(node) }
+        end
+
+        def value
+          model.to_s
+        end
+
+        private
+
+        def model
+          modelize(node, node.attributes['content'].to_s) if node
+        end
+
+        def node
+          @node ||= meta_xpath(attribute: attribute) ||
+                      meta_xpath(attribute: attribute, key: :name)
+        end
+
+        def attribute
+          "article:#{attribute_name}"
+        end
+
+        def opengraph_node?(node)
+          node.attribute('name').to_s.start_with?('article:') ||
+            node.attribute('property').to_s.start_with?('article:')
+        end
+
+        def meta
+          document.css('meta')
+        end
+
       end
     end
   end
